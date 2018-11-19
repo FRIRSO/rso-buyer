aed86f72bc58a92b137b6a2539fe8db2c49fe215
krneElL <susnikg@gmail.com>
2018-11-19 15:01:59 +0100

    services init

---
+++ services/src/main/java/si/fri/rso/projekt/buyers/services/beans/BuyersBean.java
@@ -0,0 +1,32 @@
+package si.fri.rso.projekt.buyers.services.beans;
+
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
+import si.fri.rso.projekt.buyers.services.configuration.AppProperties;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import java.util.Optional;
+
+@RequestScoped
+public class BuyersBean {
+
+    private Client httpClient;
+
+    @Inject
+    private AppProperties appProperties;
+
+    @Inject
+    private BuyersBean buyersBean;
+
+    @Inject
+    @DiscoverService("rso-orderes")
+    private Optional<String> baseUrl;
+
+    @PostConstruct
+    private void init() {
+        httpClient = ClientBuilder.newClient();
+    }
+}
---
+++ services/src/main/java/si/fri/rso/projekt/buyers/services/configuration/AppProperties.java
@@ -0,0 +1,10 @@
+package si.fri.rso.projekt.buyers.services.configuration;
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@ConfigBundle("app-properties")
+public class AppProperties {
+}
