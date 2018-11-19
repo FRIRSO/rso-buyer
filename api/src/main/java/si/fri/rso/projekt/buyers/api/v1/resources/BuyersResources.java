695ef5b4981c23483f972a2e25b538e398ee6392
krneElL <susnikg@gmail.com>
2018-11-19 16:07:49 +0100

    services init

--- api/src/main/java/si/fri/rso/projekt/buyers/api/v1/resources/BuyersResources.java
+++ api/src/main/java/si/fri/rso/projekt/buyers/api/v1/resources/BuyersResources.java
@@ -1,5 +1,8 @@
 package si.fri.rso.projekt.buyers.api.v1.resources;
 
+import si.fri.rso.projekt.buyers.services.beans.BuyersBean;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,6 +11,9 @@ import javax.ws.rs.core.Response;
 
 @Path("/buyer")
 public class BuyersResources {
+
+    @Inject
+    private BuyersBean buyersBean;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
